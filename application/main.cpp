c10a5309cf2105a6f3a557c5891efc3e2ddbd1bf
carvalheirafc <carvalheirafc@gmail.com>
2017-09-19 23:14:09 -0300

    test

--- application/main.cpp
+++ application/main.cpp
@@ -1,16 +1,33 @@
 #include "agenda.h"
 #include <memory>
 #include <vector>
-
+unsigned menu(){
+	unsigned choice;
+	std::cout<<"Setar Contatos iniciais........1"<<std::endl;
+	std::cout<<"Adicionar um novo contato......2"<<std::endl;
+	std::cout<<"Listar contatos................3"<<std::endl;
+	std::cout<<"Exportar para um arquivo CSV...4"<<std::endl;
+	std::cout<<"Exit program...................0"<<std::endl;
+	std::cin>>choice;
+	std::cin.sync();
+	std::cin.ignore();
+return choice;	
+}
 
 
 int main(){
 	
+	unsigned c;
+	bool run(true);
 	Agenda data;
-	std::cout<<"====* Insira os dados dos seus contatos *===="<<std::endl;
-	data.set_contacts();
-//	data.del_contact(1);
-	std::cout<<data;
-//	data.save_file();
+	while(run){
+		c = menu();
+		if(c == 1) data.set_contacts();
+		if(c == 2) data.add_contact();
+		if(c == 3) std::cout<<data;
+		if(c == 4) data.save_file();
+		if(c == 0) run = false;
+	}
+	
 return EXIT_SUCCESS;
 }
--- include/agenda.h
+++ include/agenda.h
@@ -7,62 +7,55 @@
 #include <memory>
 #include <vector>
 #include <locale>
-const size_t max_size = 2;
-
-
+size_t size;
+size_t max_size = 30;
 class Agenda {
 	private:
 		std::vector<Contact> _contact;
 	
 	public:
+		/** @brief Default constructor*/
 		Agenda (){
 			this->_contact.reserve(max_size);	
 		}
-		
+		/** @brief push max_size contacts */		
 		void set_contacts(){
-			for(size_t t=0;t<max_size;t++){
+			std::cout<<"Após o uso dessa função use a de adicionar contatos(2)."<<std::endl;
+			std::cout<<"Quantos contatos deseja adicionar: ";
+			std::cin>>size;
+			std::cin.sync();
+			std::cin.ignore();
+			for(size_t t=0;t<size;t++){
 				std::unique_ptr<Contact> individual = create_contact();
 				this->_contact[t] = *individual;
 			}
+		}
+		
+		/** @brief push only one contact */
+		void add_contact(){
+			for(size_t i = size; i<size+1; i++){
+				std::unique_ptr<Contact> individual = move(create_contact());
+				this->_contact[i] = *individual;	
+			}
+		size+=1;
 		}	
+		
 		void del_contact(size_t pos){
 			this->_contact.erase(this->_contact.begin() + pos);
 		}
 		
 		
 		friend std::ostream& operator <<(std::ostream& out, Agenda& right_object){
-			for(size_t k=0;k<max_size;k++){
+			for(size_t k=0;k<size;k++){
 				std::cout<<"<Contato>"<<std::endl;
 				out<<right_object._contact[k].getName()<<" -- ";
 				out<<right_object._contact[k].getDay()<<"/";
 				out<<right_object._contact[k].getMonth()<<"/";
 				out<<right_object._contact[k].getYear()<<std::endl;
+				out<<std::endl;
 			}
 		return out;
 		}
-		
-	/*	void read_file(){
-			std::fstream stream;
-			std::string path = "./";
-			std::string file_name;
-			std::string buffer;
-			
-			std::cout<<"Nome do arquivo: ";
-			std::getline(std::cin, file_name);
-			path += file_name+".dat";
-		
-			stream.open(path, std::fstream::in);
-			if(stream.is_open()){
-				for(size_t k=0; k < max_size; k++){
-					std::getline(stream, buffer);
-					sub = break_(buffer);
-					this->_contact[k].setName(buffer);			
-				}
-			
-			}		
-			stream.close();
-			
-		}*/
 		
 		
 		void save_file(){
@@ -75,14 +68,17 @@ class Agenda {
 			path += file_name+".dat";
 
 			stream.open(path, std::fstream::out);
-			for(size_t k=0; k < max_size; k++){
+			for(size_t k=0; k < size; k++){
 				stream<<this->_contact[k].getName()<<";";
 				stream<<this->_contact[k].getDay();
 				stream<<";"<<this->_contact[k].getMonth();
-				stream<<";"<<this->_contact[k].getYear()<<";endl;";
+				stream<<";"<<this->_contact[k].getYear();
 				stream<<std::endl;
 			}
 			stream.close();
+			std::cout<<std::endl;
+			std::cout<<"Aquivo salvo em: "<<path<<std::endl;
+			std::cout<<std::endl;
 		}
 		
 
